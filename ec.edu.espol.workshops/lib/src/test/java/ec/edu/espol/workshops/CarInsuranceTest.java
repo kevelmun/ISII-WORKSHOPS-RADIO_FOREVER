e23073b5bac178a64395fbff49365a9b51afb1e7
Joan Gutierrez <joangutierrez2001@gmail.com>
2022-10-13 16:03:55 -0500

    Initial commit

---
+++ ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/CarInsuranceTest.java
@@ -0,0 +1,14 @@
+package ec.edu.espol.workshops;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CarInsuranceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
