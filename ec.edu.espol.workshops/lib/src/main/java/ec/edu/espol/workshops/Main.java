49263ac761f4b1b73ef178efa42d126eac0d8c62
kevelmun <kevelmun@espol.edu.ec>
2022-10-14 22:47:54 -0500

    Corrections

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Main.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Main.java
@@ -13,93 +13,92 @@ public class Main {
 		return 0;
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {		
 		
-		Customer customer = new Customer();
-		CarInsurance insurance = new CarInsurance();
 		
 		Scanner scan = new Scanner(System.in);
 		
 		while (true) {
-			try {
-				System.out.println("Input your age:");
-				int input = Integer.parseInt(scan.next());
-				if(input >= 18 && input <= 80) {
-					customer.setAge(input);
-					break;
+			Customer customer = new Customer();
+			CarInsurance insurance = new CarInsurance();
+			while (true) {
+				try {
+					System.out.println("Input your age:");
+					int input = Integer.parseInt(scan.next());
+					if (input >= 18 && input <= 80) {
+						customer.setAge(input);
+						break;
+					}
+					System.out.println("Invalid data. Please input a valid number.");
+				} catch (Exception e) {
+					System.out.println("Invalid data. Please input a number.");
 				}
-				System.out.println("Invalid data. Please input a valid number.");
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				System.out.println("Invalid data. Please input a number.");
-			} 
-		}
-		
-		while (true) {
-			try {
-				System.out.println("Input your sex (M - F):");
-				String inputSex = scan.next();
-				if (inputSex.toUpperCase().equals("M")) {
-					customer.setSex(Sex.MALE);
-					break;
-				} else if (inputSex.toUpperCase().equals("F")) {
-					customer.setSex(Sex.FEMALE);
-					break;
-				} else {
-					System.out.println("Invalid data. Plese input a valid option");
-				}
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				System.out.println("Invalid data. Plese input a valid option.");
-			} 
-		}
-		
-		while (true) {
-			try {
-				System.out.println("Are you married? (Y - N):");
-				String inputMarried = scan.next();
-				if (inputMarried.toUpperCase().equals("Y")) {
-					customer.setMaritalStatus(true);
-					break;
-				} else if (inputMarried.toUpperCase().equals("N")) {
-					customer.setMaritalStatus(false);
-					break; 
-				} else {
-					System.out.println("Invalid data. Plese input a valid option");
-				}
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				System.out.println("Invalid data. Plese input a valid option.");
-			} 
-		}
-		
-		
-		while (true) {
-			try {
-				System.out.println("Is your driving license valid? (Y - N):");
-				String inputLicense = scan.next();
-				if (inputLicense.equals("Y")) {
-					customer.setDrivingLicence(true);
-					break;
-				} else if (inputLicense.equals("N")) {
-					customer.setDrivingLicence(false);
-					break;
-				} else {
+			}
+			while (true) {
+				try {
+					System.out.println("Input your sex (M - F):");
+					String inputSex = scan.next();
+					if (inputSex.toUpperCase().equals("M")) {
+						customer.setSex(Sex.MALE);
+						break;
+					} else if (inputSex.toUpperCase().equals("F")) {
+						customer.setSex(Sex.FEMALE);
+						break;
+					} else {
+						System.out.println("Invalid data. Plese input a valid option");
+					}
+				} catch (Exception e) {
 					System.out.println("Invalid data. Plese input a valid option.");
 				}
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				System.out.println("Invalid data. Plese input a valid option.");
+			}
+			while (true) {
+				try {
+					System.out.println("Are you married? (Y - N):");
+					String inputMarried = scan.next();
+					if (inputMarried.toUpperCase().equals("Y")) {
+						customer.setMaritalStatus(true);
+						break;
+					} else if (inputMarried.toUpperCase().equals("N")) {
+						customer.setMaritalStatus(false);
+						break;
+					} else {
+						System.out.println("Invalid data. Plese input a valid option");
+					}
+				} catch (Exception e) {
+					System.out.println("Invalid data. Plese input a valid option.");
+				}
+			}
+			while (true) {
+				try {
+					System.out.println("Is your driving license valid? (Y - N):");
+					String inputLicense = scan.next();
+					if (inputLicense.equals("Y")) {
+						customer.setDrivingLicence(true);
+						break;
+					} else if (inputLicense.equals("N")) {
+						customer.setDrivingLicence(false);
+						break;
+					} else {
+						System.out.println("Invalid data. Plese input a valid option.");
+					}
+				} catch (Exception e) {
+					System.out.println("Invalid data. Plese input a valid option.");
+				}
+			}
+			if (validateCustomer(customer) == 0) {
+				System.out.println("Your premium value is: " + insurance.calculatePremium(customer));
+				
+				
+				
+			} else {
+				System.out.println("Customer data invalid, returned -1.");
 			} 
+			System.out.println("Want to check another customer premium value? (Y/N)");
+			String action = scan.next();
+			if (action.toUpperCase().equals("N")) {
+				break;
+			}
 		}
-		
-		if(validateCustomer(customer) == 0) {
-			System.out.println("Your premium value is: " + insurance.calculatePremium(customer));
-		} else {
-			System.out.println("Customer data invalid, returned -1.");
-		}
-		
 		scan.close();
 
 	}
