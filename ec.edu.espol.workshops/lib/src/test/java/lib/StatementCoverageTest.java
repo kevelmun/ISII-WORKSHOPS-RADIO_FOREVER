cd60018cc72e496c7479988ac28dc9ab1e3cee0e
kevelmun <kevelmun@espol.edu.ec>
2022-12-22 18:57:21 -0500

    Statement Coverage Test

---
+++ ec.edu.espol.workshops/lib/src/test/java/lib/StatementCoverageTest.java
@@ -0,0 +1,121 @@
+package lib;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import ec.edu.espol.workshops.CarInsurance;
+import ec.edu.espol.workshops.Customer;
+import ec.edu.espol.workshops.Main;
+import ec.edu.espol.workshops.Sex;
+
+public class StatementCoverageTest {
+
+	@Test
+	public void tc0001() {
+		Customer cliente = new Customer();
+		cliente.setMaritalStatus(false);
+		cliente.setAge(65);
+		cliente.setSex(Sex.MALE);
+		
+		CarInsurance ci = new CarInsurance();
+		ci.setCustomer(cliente);
+		
+		double expectRes = 500.0;
+		
+		assertEquals(ci.calculatePremium(cliente), expectRes, 1);
+	
+	}
+	
+	@Test
+	public void tc0002() {
+		Customer cliente = new Customer();
+		cliente.setMaritalStatus(true);
+		cliente.setAge(20);
+		cliente.setSex(Sex.FEMALE);
+		
+		CarInsurance ci = new CarInsurance();
+		ci.setCustomer(cliente);
+		
+		double expectRes = 300.0;
+		assertEquals(ci.calculatePremium(cliente), expectRes, 1);
+	
+	}
+	
+	@Test
+	public void tc0003() {
+		Customer cliente = new Customer();
+		cliente.setMaritalStatus(false);
+		cliente.setAge(24);
+		cliente.setSex(Sex.MALE);
+		
+		CarInsurance ci = new CarInsurance();
+		ci.setCustomer(cliente);
+		
+		double expectRes = 2000.0;
+		
+		assertEquals(ci.calculatePremium(cliente), expectRes, 1);
+	
+	}
+	
+	@Test
+	public void tc0004() {
+		Customer cliente = new Customer();
+		cliente.setMaritalStatus(false);
+		cliente.setAge(50);
+		cliente.setSex(Sex.FEMALE);
+		
+		CarInsurance ci = new CarInsurance();
+		ci.setCustomer(cliente);
+		
+		double expectRes = 400.0;
+		
+		assertEquals(ci.calculatePremium(cliente), expectRes, 1);
+	
+	}
+	
+	@Test
+	public void tc0005() {
+		Customer cliente = new Customer();
+		cliente.setMaritalStatus(false);
+		cliente.setDrivingLicence(true);
+		cliente.setAge(81);
+		cliente.setSex(Sex.MALE);
+
+		int response = Main.validateCustomer(cliente); 
+		int expectRes = -1;
+		
+		assertEquals(expectRes, response);
+	
+	}
+
+	@Test
+	public void tc0006() {
+		Customer cliente = new Customer();
+		cliente.setMaritalStatus(false);
+		cliente.setDrivingLicence(false);
+		cliente.setAge(70);
+		cliente.setSex(Sex.MALE);
+
+		int response = Main.validateCustomer(cliente); 
+		int expectRes = -1;
+		
+		assertEquals(expectRes, response);
+	}
+
+	@Test
+	public void tc0007() {
+		Customer cliente = new Customer();
+		cliente.setMaritalStatus(false);
+		cliente.setDrivingLicence(true);
+		cliente.setAge(79);
+		cliente.setSex(Sex.MALE);
+
+		int response = Main.validateCustomer(cliente); 
+		int expectRes = 0;
+		
+		assertEquals(expectRes, response);
+	}
+
+
+}
